28306bbd002d9853e154b082186e9612a0ebe49d
Mr-Write <97372824+Mr-Write@users.noreply.github.com>
2022-07-27 14:16:13 +0800

    Update SpringTestApplication.java

--- src/main/java/com/example/springtest/SpringTestApplication.java
+++ src/main/java/com/example/springtest/SpringTestApplication.java
@@ -11,5 +11,6 @@ public class SpringTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringTestApplication.class, args);
         System.out.println("I have modify");
+        System.out.println("I modify again")
     }
 }
